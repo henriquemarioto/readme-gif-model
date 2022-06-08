25273c03568a194b982fc26e970ec2f5bdbfc206
henriquemarioto <henrique.marioto@outlook.com>
2022-06-08 15:38:31 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Container, Text } from "./style";
+import GlobalStyle from "./styles/global";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <Text>Paulo Marioto</Text>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    position: relative;
+
+    height: 100%;
+    width: 100%;
+`;
+
+export const Text = styled.h1`
+    color: #fff;
+    font-size: 42px;
+`;
